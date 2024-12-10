f576b513fd725d2dc81f2cd6b98b6eca5fef6af6
oo29197 <chuaqiyuan@icloud.com>
2024-12-10 10:20:43 +0800

    read 50000000 doc

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read his docs
+            Read 50000000 docs
           </a>
         </div>
       </main>
