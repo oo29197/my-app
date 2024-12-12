9b0fa561604dca85ba11f57cfac977d0ed85a337
minnong <ongminn@gmail.com>
2024-12-12 17:03:33 +0800

    fix: card

--- components/3dcard/3dcard.tsx
+++ components/3dcard/3dcard.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
+    Card,
+    CardDescription,
+    CardHeader,
+    CardTitle,
 } from "@/components/ui/card";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -13,7 +13,6 @@ import General from "/public/general.gif";
 
 export function ThreeDCardDemo() {
   const router = useRouter();
-
   const handleRoute = () => {
     router.push("/quiz");
   };
@@ -21,13 +20,20 @@ export function ThreeDCardDemo() {
   return (
     <CardContainer className="w-full max-w-5xl mx-auto">
       <CardBody className="flex flex-col justify-center bg-green-900 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] sm:w-[50rem] h-auto rounded-xl p-6 border  ">
-        <Card className="text-center border-none shadow-none bg-green-900 rounded-none">
-          <CardHeader>
-            <CardTitle className="text-green-500">Feeling stressed?</CardTitle>
-            <CardDescription className="text-green-500">
-              In that case, I would never recommend the use of drugs.
-            </CardDescription>
-          </CardHeader>
+        <Card className="text-center flex justify-center border-none shadow-none bg-green-900 rounded-none">
+          <CardItem
+            translateZ="50"
+            className="text-xl font-bold text-neutral-600 dark:text-white"
+          >
+            <CardHeader>
+              <CardTitle className="text-green-500">
+                Feeling stressed?
+              </CardTitle>
+              <CardDescription className="text-green-500">
+                In that case, I would never recommend the use of drugs.
+              </CardDescription>
+            </CardHeader>
+          </CardItem>
         </Card>
 
         <div className="flex justify-center">
@@ -35,17 +41,18 @@ export function ThreeDCardDemo() {
         </div>
 
         <Card className="text-center flex justify-center border-none shadow-none bg-green-900 rounded-none">
-          <CardHeader>
-            <CardDescription className="text-green-500">
-              "NO SUBSTANCE ABUSE IN MY BASE!"
-            </CardDescription>
-          </CardHeader>
+          <CardItem translateZ="50">
+            <CardHeader>
+              <CardDescription className="text-green-500">
+                "NO SUBSTANCE ABUSE IN MY BASE!"
+              </CardDescription>
+            </CardHeader>
+          </CardItem>
         </Card>
 
         <div className="flex justify-center items-center" onClick={handleRoute}>
           <CardItem
-            translateZ={20}
-            translateX={40}
+            translateZ="50"
             as="button"
             className="px-4 py-2 rounded-xl bg-cyan-900 text-cyan-400 text-md  p-4 font-bold"
           >
